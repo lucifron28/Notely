1dd8ce45e6f5474509cdd7e7709f8734741294b9
ron <lucifron28@gmail.com>
2025-10-22 22:03:38 +0800

    fix: update response status and message for note update

--- backend/src/controllers/notesController.js
+++ backend/src/controllers/notesController.js
@@ -45,7 +45,7 @@ export const updateNote = async (req, res) => {
     if (!result) {
       return res.status(404).json({ message: "Note not found" });
     }
-    res.status(201).json({ message: "Note updated successfully!" });
+    res.status(200).json(result);
   } catch (error) {
     console.log(`updateNote Controller Error: ${error}`);
     res.status(500).json({ message: "Internal Server Error" });
