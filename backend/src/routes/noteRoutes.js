ec12c069d4204b072f2480b76187a0784e34b788
ron <lucifron28@gmail.com>
2025-10-22 18:08:11 +0800

    feat: add routes for getting a note by ID and deleting all notes

--- backend/src/routes/noteRoutes.js
+++ backend/src/routes/noteRoutes.js
@@ -4,13 +4,17 @@ import {
   createNote,
   updateNote,
   deleteNote,
+  deleteAllNote,
+  getNote,
 } from "../controllers/notesController.js";
 
 const router = express.Router();
 
 router.get("/", getAllNotes);
+router.get("/:id", getNote);
 router.post("/", createNote);
 router.put("/:id", updateNote);
 router.delete("/:id", deleteNote);
+router.delete("/", deleteAllNote);
 
 export default router;
