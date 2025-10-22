7f9ca7bb5bdb872b2666ba9dd3c2fa607273426f
ron <lucifron28@gmail.com>
2025-10-23 02:19:14 +0800

    feat: integrate rate limiter middleware for enhanced API request control

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import router from "./routes/noteRoutes.js";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
+import rateLimiter from "./middleware/rateLimiter.js";
 
 dotenv.config();
 
@@ -10,12 +11,12 @@ const app = express();
 const PORT = process.env.PORT || 5001;
 
 app.use(express.json());
+app.use(rateLimiter);
 app.use("/api/notes", router);
 
-connectDB();
-
-app.listen(PORT, () => {
-  console.log(`Server started on PORT: ${PORT}`);
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server started on PORT: ${PORT}`);
+  });
 });
-
 
