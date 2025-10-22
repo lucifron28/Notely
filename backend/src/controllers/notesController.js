6f4dfa6c015d369ee169123b0aace9d1650c07a6
ron <lucifron28@gmail.com>
2025-10-22 14:11:00 +0800

    feat: fetch all notes for allNotesController

--- backend/src/controllers/notesController.js
+++ backend/src/controllers/notesController.js
@@ -1,5 +1,13 @@
-export const getAllNotes = (req, res) => {
-  res.status(200).send("You just fetched the notes");
+import Note from "../models/Note.js";
+
+export const getAllNotes = async (req, res) => {
+  try {
+    const notes = await Note.find();
+    res.status(200).json(notes);
+  } catch (error) {
+    console.log(`getAllNotes Controller Error: ${error}}`);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 };
 
 export const createNote = (req, res) => {
