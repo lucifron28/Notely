1c86c9138a409b1cd08439448748ae39d8b557f4
ron <lucifron28@gmail.com>
2025-10-24 12:14:29 +0800

    fix: improve error handling in HomePage component and enhance loading UI

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -3,6 +3,7 @@ import RateLimitedUI from "../components/RateLimitedUI.jsx";
 import { useState, useEffect } from "react";
 import axios from "axios";
 import toast from "react-hot-toast";
+import NoteCard from "../components/NoteCard.jsx";
 
 const HomePage = () => {
   const [isRateLimited, setIsRateLimited] = useState(false);
@@ -14,10 +15,11 @@ const HomePage = () => {
       try {
         const res = await axios.get("http://localhost:5001/api/notes");
         setNotes(res.data);
+        console.log(res.data); // Log the actual response
         setIsRateLimited(false);
       } catch (error) {
         console.log(`Error fetching notes: ${error}`);
-        if (error.response.status == 429) {
+        if (error.response && error.response.status === 429) {
           setIsRateLimited(true);
         } else {
           toast.error("Failed to load notes");
@@ -27,11 +29,25 @@ const HomePage = () => {
       }
     };
     fetchNotes();
-  });
+  }, []);
+
   return (
     <div className="min-h-screen">
       <Navbar />
       {isRateLimited && <RateLimitedUI />}
+      {loading ? (
+        <div className="flex items-center justify-center min-h-screen">
+          <span className="loading loading-dots loading-xl text-primary"></span>
+        </div>
+      ) : (
+        <div className="container mx-auto py-20">
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5">
+            {notes.map((note) => (
+              <NoteCard key={note._id} note={note} />
+            ))}
+          </div>
+        </div>
+      )}
     </div>
   );
 };
